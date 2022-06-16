6933b10955ccfe935ff4cb2221b16e82d9faab38
aykhanmv <92543867+aykhanmv@users.noreply.github.com>
2022-06-16 20:41:26 +0400

    changed setting

--- academy/settings.py
+++ academy/settings.py
@@ -63,8 +63,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'academy.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 if DEBUG:
     DATABASES = {
         'default': {
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
