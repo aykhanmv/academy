3a8d33eeed0049d4895624daccfa586391f45650
aykhanmv <92543867+aykhanmv@users.noreply.github.com>
2022-06-16 20:33:53 +0400

    changed setting

--- academy/settings.py
+++ academy/settings.py
@@ -65,24 +65,24 @@ WSGI_APPLICATION = 'academy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'academydb',
+        'USER': 'academy_admin',
+        'PASSWORD': 'Aykhan@6446',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'academydb',
-            'USER': 'academy_admin',
-            'PASSWORD': 'Aykhan@6446',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 
 
 
