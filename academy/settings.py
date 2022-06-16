102575f9f1c015609f9db40f9c0f7d451bde2038
aykhanmv <92543867+aykhanmv@users.noreply.github.com>
2022-06-16 19:11:09 +0400

    debug false

--- academy/settings.py
+++ academy/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['157.245.73.39', 'www.blokchainacademy.az', 'blokchainacademy.az', ]
 
