cdd16a065fff27f91c5b43e104cae89481bee4db
aykhanmv <92543867+aykhanmv@users.noreply.github.com>
2022-06-16 21:24:12 +0400

    changed setting

--- academy/settings.py
+++ academy/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iis98+1ruxamj@30%ij(p463ndn7qtv*=6wjo+u#4)zdhx(5g7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['157.245.73.39', 'blockhainacademy.az', 'www.blockhainacademy.az', ]
 
