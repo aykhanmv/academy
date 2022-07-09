dcd9b8db17fe5620021e2541b89582a9699b761b
aykhanmv <92543867+aykhanmv@users.noreply.github.com>
2022-07-09 20:29:40 +0400

    missed reqs

--- users/views.py
+++ users/views.py
@@ -1,17 +1,57 @@
-from pydoc import render_doc
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.http import HttpResponse  
+from django.shortcuts import render, redirect  
+from django.contrib.auth import login, authenticate  
+from django.contrib.sites.shortcuts import get_current_site  
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
+from django.template.loader import render_to_string  
+from .token import account_activation_token
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User  
+from django.core.mail import EmailMessage  
+
 
 def register (request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'{username} üçün hesab uğurla yaradıldı.') 
-            return redirect('login')
+            user = form.save(commit=False)  
+            user.is_active = False  
+            user.save()
+            
+            current_site = get_current_site(request)  
+            mail_subject = 'Hesabın doğrulanması linki'
+            message = render_to_string('users/acc_active_email.html', {  
+                'user': user,  
+                'domain': current_site.domain,  
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
+                'token':account_activation_token.make_token(user),  
+            })  
+
+            to_email = form.cleaned_data.get('email')  
+            email = EmailMessage(  
+                mail_subject, message, to=[to_email]  
+            )
+            email.send()
+            return render(request, 'users/verification_sent.html', {'title': 'Blockchain Academy'})
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+def activate(request, uidb64, token):  
+    User = get_user_model()
+    try:  
+        uid = force_str(urlsafe_base64_decode(uidb64))  
+        user = User.objects.get(pk=uid)  
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):  
+        user = None  
+    if user is not None and account_activation_token.check_token(user, token):  
+        user.is_active = True  
+        user.save()  
+        return render(request, 'users/account_verified.html', {'title': 'Blockchain Academy'})  
+    else:  
+        return render(request, 'users/account_wrong_link.html', {'title': 'Blockchain Academy'})
